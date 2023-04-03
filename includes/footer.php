94287ce253f26edf07ed61bcef335deed1798713
Hamza A <attarwalahamza@gmail.com>
2023-04-03 18:25:36 -0400

    updating footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,24 +1,38 @@
 <footer>
         <div class="linksnsocial">
             <section class="links">
-            <h2>Links</h2>
             <ul>
-                <li><a href="https://trentcentral.ca/">Trent Central Student Association (TCSA)</a></li>
-                <li><a href="https://www.mytdsa.ca/">Trent Durham Student Association</a></li>
-                <li><a href="https://trentgsa.ca/">Trent Graduate Student Association</a></li>
-                <li><a href="https://www.trentu.ca/housing/">Trent Housing</a></li>
-                <li><a href="https://www.trentu.ca/library/frontpage">Bata Library - Trent University Library & Archives</a></li>
-                <li><a href="https://www.bkstr.com/trentstore/home">Trent Bookstore</a></li>
+                <h2><i class="fa-solid fa-link"></i> Links</h2>
+                <div id = "foot">
+                    <div>
+                        <li><a href="https://trentcentral.ca/">Trent Central Student Association (TCSA)</a></li>
+                        <li><a href="https://www.mytdsa.ca/">Trent Durham Student Association</a></li>
+                        <li><a href="https://trentgsa.ca/">Trent Graduate Student Association</a></li>
+                    </div>
+                    <div>
+                        <li><a href="https://www.trentu.ca/housing/">Trent Housing</a></li>
+                        <li><a href="https://www.trentu.ca/library/frontpage">Bata Library - Trent University Library & Archives</a></li>
+                        <li><a href="https://www.bkstr.com/trentstore/home">Trent Bookstore</a></li>
+                    </div>
+                </div>
             </ul>
             </section>
             <section class="socials">
-                <h2>Socials</h2>
                 <ul>
-                    <li><a href="https://www.facebook.com/trentuniversity/">Facebook -<i class="fa-brands fa-facebook"></i> </a></li>
-                    <li><a href="https://www.instagram.com/trentuniversity/?hl=en">Instagram -<i class="fa-brands fa-instagram"></i> </a></li>
-                    <li><a href="https://twitter.com/TrentUniversity?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">Twitter -<i class="fa-brands fa-twitter"></i></a></li>
-                    <li><a href="https://www.tiktok.com/@trent.university?lang=en">TikTok - <i class="fa-brands fa-tiktok"></i></a></li>
-                    <li><a href="https://www.reddit.com/r/TrentUniversity/">Reddit - <i class="fa-brands fa-reddit"></i></a></li>
+                    <h2><i class="fa-solid fa-people-arrows"></i> Socials</h2>
+                    <div id = "socials">
+                        <div>
+                            <li><a href="https://www.facebook.com/trentuniversity/"><i class="fa-brands fa-facebook"></i> Facebook</a></li>
+                            <li><a href="https://www.instagram.com/trentuniversity/?hl=en"><i class="fa-brands fa-instagram"></i> Instagram</a></li>
+                        </div>
+                        <div>
+                            <li><a href="https://twitter.com/TrentUniversity"><i class="fa-brands fa-twitter"></i> Twitter</a></li>
+                            <li><a href="https://www.tiktok.com/@trent.university?lang=en"><i class="fa-brands fa-tiktok"></i> TikTok</a></li>
+                        </div>
+                        <div>
+                            <li><a href="https://www.reddit.com/r/TrentUniversity/"><i class="fa-brands fa-reddit"></i> Reddit</a></li>
+                        </div>
+                    </div>
                 </ul>
             </section>
         </div>
