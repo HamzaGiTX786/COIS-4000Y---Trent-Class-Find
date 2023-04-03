44a5cc28a004f80921bf432664f0a7ac505c4b80
Hamza A <attarwalahamza@gmail.com>
2023-04-03 18:24:48 -0400

    updating nav

--- includes/nav.php
+++ includes/nav.php
@@ -1,7 +1,8 @@
 <nav>
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="pickaroom.php">Pick a Room</a></li>
-            <li><a href="admin.php">Admin Login</a></li>
-        </ul>
-    </nav>
+    <ul>
+        <li id = "navmenu"><i class = "fa-solid fa-angle-down"></i>Menu</li>
+        <li><a href="index.php"><i class="fa-solid fa-house"></i> Home</a></li>
+        <li><a href="pickaroom.php"><i class="fa-solid fa-person-shelter"></i> Pick a Room</a></li>
+        <li><a href="admin.php"><i class="fa-solid fa-lock"></i> Admin Login</a></li>
+    </ul>
+</nav>
